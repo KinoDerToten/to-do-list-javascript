4d8aa6836ff40d03adddd30779dbe9a71a63499f
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-09-12 23:06:24 -0400

    criando a logica

--- todolist.js
+++ todolist.js
@@ -1,5 +1,8 @@
-document.getElementById("addTask").onkeyup = newTask(event);
-
-const newTask = (e) => {
-
-};
+function newTask(e) {
+	if (e.keyCode == 13) {
+		let task = document.getElementById("addTask").value;
+		let novaTask = document.getElementById("newTask");
+		let aTask = document.createTextNode(task);
+		novaTask.appendChild(aTask);
+	};
+}
