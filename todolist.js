5138ac5fa2a7fb5ae57d3194957c100db7f822d4
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2021-10-02 12:32:54 -0400

    criando a logica

---
+++ todolist.js
@@ -0,0 +1,21 @@
+const newTask = document.getElementById("newTask");
+const tasks = document.getElementById("tasks");
+
+const addTask = (e) => {
+    if (e.keyCode == 13) {
+        let taskEl = document.createElement("li");
+        let taskElContainer = document.createElement("label");
+        let checkbox = document.createElement("input");
+        let checkboxSpan = document.createElement("span");
+        tasks.appendChild(taskEl);
+        taskElContainer.setAttribute("id", "checked");
+        taskElContainer.classList.add("container-checked")
+        taskEl.appendChild(taskElContainer);
+        checkbox.setAttribute("type","checkbox");
+        checkboxSpan.classList.add("checkmark");
+        taskElContainer.appendChild(checkbox);
+        taskElContainer.appendChild(checkboxSpan);
+        taskElContainer.appendChild(newTask.value);
+        
+    }
+};
